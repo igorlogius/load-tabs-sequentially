c395112d0d23c3c3ced25cabf9fb2a0f487b53ca
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-03-28 21:11:04 +0200

    fix(#1): expand sequencial loading onto none newly created tabs

--- background.js
+++ background.js
@@ -1,103 +1,76 @@
 
 (async () => {
+    const proxyURL = browser.runtime.getURL("/tab.html?url=");
 	const manifest = browser.runtime.getManifest();
 	const extname = manifest.name;
-
-	let queuedTabs = [];
-	let wakingTab = -1;
 
 	const filter = (await (async  () => {
 		const info = await browser.runtime.getBrowserInfo();
 		const major_version = info.version.split(".")[0];
-		return { properties: [((major_version < 88)?'status':'url')] };
+		return { properties: ((major_version < 88)?['status']:['status','url']) };
 	})());
 
-	function handleCreated(tab) {
-		//console.log('handleCreated');
-		//if(!tab.active) {
-			queuedTabs.push(tab.id);
-		//}
-	}
-
-	/*
-	function handleCreatedNavigationTarget(){
-		console.log('handleCreatedNavigationTarget');
-	}
-	*/
-
-	function delFromQedTabs(id) {
-		if(queuedTabs.includes(id)) {
-			queuedTabs.splice(queuedTabs.indexOf(id), 1);
-		}
-	}
-
+    // identify the currently loading tab
+    let tabCurrentlyLoading = -1;
 
 	function handleUpdated(tabId, changeInfo, tabInfo){
+        if( tabId === tabCurrentlyLoading
+            && changeInfo.status === 'complete'){
+            // reset currently loading tab, when it completed its loading state
+            // will let sequencialLoad know that it can release the next tab if there is any
+            tabCurrentlyLoading = -1;
+        }else
 		if(
 			!tabInfo.active
-			&& changeInfo.url !== undefined 
-			&& queuedTabs.includes(tabId) 
-			&& !changeInfo.url.startsWith("moz-extension://") 
-			&& !changeInfo.url.startsWith("about:") 
-			&& !changeInfo.url.startsWith(browser.runtime.getURL("/tab.html?url=")) 
+			&& changeInfo.url !== undefined
+            && tabId !== tabCurrentlyLoading
+			&& !changeInfo.url.startsWith("moz-extension://")
+			&& !changeInfo.url.startsWith("about:")
+			&& !changeInfo.url.startsWith(proxyURL)
 		){
 			browser.tabs.update(tabId, { url: "tab.html?url=" + changeInfo.url });
 		}
 	}
 
-	async function handleActivated(activeInfo){
-		const tab = await browser.tabs.get(activeInfo.tabId);
-		if(tab.url.startsWith(browser.runtime.getURL("/tab.html?url="))){
+    async function handleActivated(activeInfo){
+        const tab = await browser.tabs.get(parseInt(activeInfo.tabId));
+        if(tab.url.startsWith(proxyURL)){
+            browser.tabs.update(activeInfo.tabId, {
+                url: tab.url.split("/tab.html?url=")[1]
+            });
+        }
+    }
 
-			delFromQedTabs(activeInfo.tabId);
-			browser.tabs.update(activeInfo.tabId, {
-				url: tab.url.split("/tab.html?url=")[1]
-			});
+    // to not block forever if a tab get closed while it is still loading
+    function handleRemoved(tabId, removeInfo) {
+        if(tabId === tabCurrentlyLoading){
+            tabCurrentlyLoading = -1;
+        }
+    }
 
-		}
-	}
-
-
-	function handleRemoved(tabId, removeInfo) {
-		delFromQedTabs(tabId);
-		if(wakingTab === tabId) {
-			wakingTab = -1;
-			//console.log('released lock on wakingTab ', tabId);
-		}
-	}
-
-	function onCompleted (details) {
-		if(!details.url.startsWith(browser.runtime.getURL("/tab.html?url="))){
-			handleRemoved(details.tabId,null);
-			//console.log('finished loading ', details.url);
-		}
-	}
-
-	async function checkManagedTabs() {
-
-		if(queuedTabs.length > 0) {
-			const tid = queuedTabs[0];
-
-			if(wakingTab !== tid){
-				//console.log('blocking lock on wakingTab ', tabId);
-				const tab = await browser.tabs.get(tid);
-
-				if(tab.url.startsWith(browser.runtime.getURL("/tab.html?url="))){
-					wakingTab = tid;
-					//console.log('set lock on wakingTab ', tid);
-					delFromQedTabs(tid);
-					//console.log('started loading ', tab.url.split("/tab.html?url=")[1]);
-					browser.tabs.update(tid, { url: tab.url.split("/tab.html?url=")[1] });
-
-
-				}
-			}
-		}
-
+    // load tab(s) sequencialy
+    async function sequencialLoad() {
+        if(tabCurrentlyLoading !== -1){
+            return;
+        }
+        const queryObj = {
+            active: false,
+            discarded: false,
+            hidden:false,
+        };
+        const tabsToBeLoaded = (await browser.tabs.query(queryObj)).sort((a,b) => { return a.index-b.index;});
+        for(const tab of tabsToBeLoaded){
+            if(tab.url.startsWith(proxyURL)){
+                tabCurrentlyLoading = tab.id;
+                browser.tabs.update(tab.id, { url: tab.url.split("/tab.html?url=")[1] });
+                break;
+            }
+        }
+    }
 
 	const delaytime = (await (async () => {
 		try {
-			let tmp = await getFromStorage('number','delay', 1500);
+			let tmp = await getFromStorage('number','delay', 2500);
 			//console.log('tmp ', typeof tmp);
 
 			tmp = parseInt(tmp);
@@ -109,26 +82,23 @@
 		}catch(e){
 			console.error(e);
 		}
-		return 1500;
+		return 2500;
 
 	})());
-		console.log(delaytime);
-		setTimeout(checkManagedTabs,delaytime);
-	}
 
+	console.log('delaytime: ', delaytime);
+	setInterval(sequencialLoad,delaytime);
 
-
-	browser.tabs.onCreated.addListener(handleCreated);
 	browser.tabs.onActivated.addListener(handleActivated);
 	browser.tabs.onUpdated.addListener(handleUpdated, filter);
-	browser.webNavigation.onCompleted.addListener(onCompleted);
-	browser.tabs.onRemoved.addListener(handleRemoved);
+    browser.tabs.onRemoved.addListener(handleRemoved);
+
 
 	async function getFromStorage(type,id,fallback) {
 		let tmp = await browser.storage.local.get(id);
 		return (typeof tmp[id] === type) ? tmp[id] : fallback;
 	}
 
-	checkManagedTabs();
+	sequencialLoad();
 
 })();
